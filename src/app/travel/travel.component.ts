8fc553430e39f65bdbc01a2df53a595df84ac4c2
craigbauer89 <craigbauer89@gmail.com>
2023-07-13 00:28:59 +0200

    Airport buttons

--- src/app/travel/travel.component.ts
+++ src/app/travel/travel.component.ts
@@ -7,9 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TravelComponent implements OnInit {
 
+  areYouSure = false;
+  modifybox = false;
+  linate = false;
+  bergamo = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  close() {
+    this.modifybox = false;
+    this.linate = false;
+    this.bergamo = false;
+  
+  }
+
+  modifyPartita() {
+    this.modifybox = true;
+    this.linate = false;
+    this.bergamo = false;
+ 
+
+   }
+
+
+
+   linatebox() {
+    this.linate = true;
+    this.modifybox = false;
+    this.bergamo = false;
+ 
+
+   }
+
+   bergamobox() {
+    this.bergamo = true;
+    this.modifybox = false;
+    this.linate = false;
+ 
+  
+ 
+
+   }
 
 }
